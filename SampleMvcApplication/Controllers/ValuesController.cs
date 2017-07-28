b757d2c47dd1e4f9faf7b99c919e55f32a4fd378
Venkata Yerrapothu <YerrapothuVenkata@JohnDeere.com>
2017-07-28 16:31:57 -0500

    Okta related changes.

---
+++ SampleMvcApplication/Controllers/ValuesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Web.Http;
+
+namespace SampleMvcApplication.Controllers
+{
+    [Authorize]
+    public class ValuesController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+
+            var identity = System.Web.HttpContext.Current.User.Identity as ClaimsIdentity;
+            return new string[] { identity.Claims.First().Issuer, identity.Claims.FirstOrDefault(c => c.Type == "Lastname").Value };
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- SampleMvcApplication/Global.asax.cs
+++ SampleMvcApplication/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Http;
 
 namespace SampleMvcApplication
 {
@@ -13,9 +14,11 @@ namespace SampleMvcApplication
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+             
         }
     }
 }
